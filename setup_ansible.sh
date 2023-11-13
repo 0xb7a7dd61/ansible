161caa82d45d2488d6d2457dd6a872bbc8745631
0xb7a7dd61 <t1dev@icloud.com>
2023-11-13 10:44:31 -0700

    feat: install system programs and dotfiles with ansible

---
+++ setup_ansible.sh
@@ -0,0 +1,15 @@
+if command -v ansible >/dev/null 2>&1; then
+	echo "Ansible already installed"
+	exit 0
+fi
+cwd=$(pwd)
+cd /opt
+sudo mkdir homebrew
+sudo chown $USER homebrew
+sudo chgrp admin homebrew
+curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
+cd $cwd
+
+export PATH="/opt/homebrew/bin:$PATH"
+brew install ansible
+ansible-galaxy collection install community.general
